584cdd73c1a70f6078211349368ee192d0ccc81e
jamal-eagle <jamalalmrshed1999@gmail.com>
2025-03-27 01:46:16 +0300

    secound  middelware

--- app/Http/Middleware/CheckResturant.php
+++ app/Http/Middleware/CheckResturant.php
@@ -4,17 +4,16 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class CheckResturant
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
+
     public function handle(Request $request, Closure $next): Response
     {
+        if(Auth::user()->role === 'resturant')
         return $next($request);
+        return response()->json(['message'=> 'Unauthanteceted'], 403);
     }
 }
